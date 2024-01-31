0f6511e8c5babff57ce3cddc2fa0fbd7702d63df
Vlad <Smirnov.ru92@mail.ru>
2024-01-31 19:04:12 +0500

    template for collections example

--- rocketView.js
+++ rocketView.js
@@ -1,4 +1,46 @@
 const RocketView = Backbone.View.extend({
+    // events: {
+    //     "click .changeSize": "changeSize",
+    //     "click .deleteRow": "deleteRow",
+    //     "blur .desc": "editValue",
+    //     "blur .name": "editValue",
+    //     "blur .size": "editValue",
+    // },
+    //
+    // initialize() {
+    //     this.template = _.template($('#viewRocket').html())
+    //     this.listenTo(this.model, "change", this.render)
+    //     this.listenTo(this.model, "destroy", this.remove)
+    //     this.render();
+    // },
+    //
+    // deleteRow() {
+    //     this.model.destroy()
+    // },
+    //
+    // editValue() {
+    //     // debugger
+    //     this.model.set({
+    //         name: this.$('.name').text(),
+    //         description: this.$('.desc').text(),
+    //         size:parseInt(this.$('input.size').attr('value'))
+    //     })
+    // },
+    //
+    // changeSize(event){
+    //     this.model.set({
+    //         size: this.model.get('size') + +event.target.dataset.rel
+    //     },{validate:true})
+    // },
+    //
+    // render() {
+    //     const json = this.model.toJSON()
+    //     const view = this.template(json)
+    //     this.$el.html(view)
+    //     console.log(json)
+    // }
+
+
     events: {
         "click .changeSize": "changeSize",
         "click .deleteRow": "deleteRow",
@@ -8,9 +50,7 @@ const RocketView = Backbone.View.extend({
     },
 
     initialize() {
-        this.template = _.template($('#viewRocket').html())
-        this.listenTo(this.model, "change", this.render)
-        this.listenTo(this.model, "destroy", this.remove)
+        this.template = _.template($('#viewRockets').html())
         this.render();
     },
 
@@ -39,4 +79,6 @@ const RocketView = Backbone.View.extend({
         this.$el.html(view)
         console.log(json)
     }
+
+
 });
